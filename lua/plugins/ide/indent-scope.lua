a57961a60f67d812dc5a3a5c3ebe1c48d143e821
Noah Sedlik <noahsedlik@berkeley.edu>
2025-06-14 19:51:36 -0700

    add indent scope

---
+++ lua/plugins/ide/indent-scope.lua
@@ -0,0 +1,27 @@
+return {
+	"echasnovski/mini.indentscope",
+	config = function()
+		local indentscope = require("mini.indentscope")
+		indentscope.setup({
+			draw = {
+				-- Delay (in ms) between event and start of drawing scope indicator
+				delay = 100,
+
+				-- 2ms time to draw each line
+				animation = function(s, n)
+					return 2
+				end,
+				--minidoc_replace_end
+
+				-- Whether to auto draw scope: return `true` to draw, `false` otherwise.
+				-- Default draws only fully computed scope (see `options.n_lines`).
+				predicate = function(scope)
+					return not scope.body.is_incomplete
+				end,
+
+				-- Symbol priority. Increase to display on top of more symbols.
+				priority = 2,
+			},
+		})
+	end,
+}
