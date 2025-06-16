301a336f237098829b87c2ef855c3db7281db199
Noah Sedlik <noahsedlik@berkeley.edu>
2025-06-16 10:22:39 -0700

    add Neotree open buffer and show certain dotfiles

--- lua/plugins/ide/neo-tree.lua
+++ lua/plugins/ide/neo-tree.lua
@@ -8,12 +8,47 @@ return {
 	},
 	config = function()
 		-- For full list of configs: https://github.com/nvim-neo-tree/neo-tree.nvim
-		vim.keymap.set("n", "<leader>b", ":Neotree source=buffers position=float<CR>", { desc = "Buffers" })
+		vim.keymap.set("n", "<C-w>b", ":Neotree source=buffers position=float<CR>", { desc = "View Open Buffers" })
 		vim.keymap.set(
 			"n",
 			"\\",
 			":Neotree source=filesystem position=right reveal=true action=show toggle<CR>",
 			{ desc = "Neotree Open" }
 		)
+		vim.keymap.set(
+			"n",
+			"<C-w>\\",
+			":Neotree source=filesystem position=right reveal=true<CR>",
+			{ desc = "Jump to Neotree" }
+		)
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			filesystem = {
+				filtered_items = {
+					visible = false, -- when true, they will just be displayed differently than normal items
+					hide_dotfiles = true,
+					hide_by_name = {
+						--"node_modules"
+					},
+					hide_by_pattern = { -- uses glob style patterns
+						--"*/src/*/tsconfig.json",
+					},
+					always_show = { -- remains visible even if other settings would normally hide it
+						".gitignore",
+					},
+					always_show_by_pattern = { -- uses glob style patterns
+						".bash*",
+						"*.conf",
+					},
+					never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+						--".DS_Store",
+						--"thumbs.db"
+					},
+					never_show_by_pattern = { -- uses glob style patterns
+						--".null-ls_*",
+					},
+				},
+			},
+		})
 	end,
 }
